b9bf72e2dedf577d0430830822534ae1742ca9c3
guncloud <hankseur@gmail.com>
2019-08-05 11:00:30 +0700

    update rest-api version

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var environment = process.env.ENV || 'dev';
 var options = {
   workers: Number(argv.w) || Number(process.env.SOCKETCLUSTER_WORKERS) || 1,
   brokers: Number(argv.b) || Number(process.env.SOCKETCLUSTER_BROKERS) || 1,
-  port: Number(argv.p) || Number(process.env.SOCKETCLUSTER_PORT) || 5000,
+  port: Number(argv.p) || Number(process.env.SOCKETCLUSTER_PORT) || Number(process.env.PORT) || 5000,
   // You can switch to 'sc-uws' for improved performance.
   wsEngine: process.env.SOCKETCLUSTER_WS_ENGINE || 'ws',
   appName: argv.n || process.env.SOCKETCLUSTER_APP_NAME || null,
